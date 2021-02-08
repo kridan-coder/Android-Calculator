bdee7995be7d8660365b2d8a63615aeb6d447b11
kridan-coder <krivelevdaniil@gmail.com>
2021-02-08 20:18:43 +0700

    buttons init

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.example.calculator
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button0: Button
+    private lateinit var button1: Button
+    private lateinit var button2: Button
+    private lateinit var button3: Button
+    private lateinit var button4: Button
+    private lateinit var button5: Button
+    private lateinit var button6: Button
+    private lateinit var button7: Button
+    private lateinit var button8: Button
+    private lateinit var button9: Button
+
+    private lateinit var buttonPlus: Button
+    private lateinit var buttonMinus: Button
+    private lateinit var buttonMultiply: Button
+    private lateinit var buttonDivide: Button
+    private lateinit var buttonModulo: Button
+    private lateinit var buttonPlusMinus: Button
+
+    private lateinit var buttonEqual: Button
+
+    private lateinit var buttonAC: Button
+
+    private lateinit var buttonComma: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button0 = findViewById(R.id.button_0)
+        button1 = findViewById(R.id.button_1)
+        button2 = findViewById(R.id.button_2)
+        button3 = findViewById(R.id.button_3)
+        button4 = findViewById(R.id.button_4)
+        button5 = findViewById(R.id.button_5)
+        button6 = findViewById(R.id.button_6)
+        button7 = findViewById(R.id.button_7)
+        button8 = findViewById(R.id.button_8)
+        button9 = findViewById(R.id.button_9)
+
+        buttonPlus = findViewById(R.id.button_plus)
+        buttonMinus = findViewById(R.id.button_minus)
+        buttonMultiply = findViewById(R.id.button_multiply)
+        buttonDivide = findViewById(R.id.button_divide)
+        buttonModulo = findViewById(R.id.button_percent)
+        buttonPlusMinus = findViewById(R.id.button_plusminus)
+
+        buttonEqual = findViewById(R.id.button_equal)
+        buttonAC = findViewById(R.id.button_AC)
+
+        buttonComma = findViewById(R.id.button_comma)
+
     }
+
+
 }
