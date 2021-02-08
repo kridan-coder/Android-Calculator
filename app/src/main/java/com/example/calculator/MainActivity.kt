a9205b443d78652c2361f2ae13f475284547681f
kridan-coder <krivelevdaniil@gmail.com>
2021-02-08 21:48:21 +0700

    digits are shown correctly on the screen

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -6,10 +6,11 @@ import android.os.Bundle
 import android.util.AttributeSet
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var button0: Button
+/*    private lateinit var button0: Button
     private lateinit var button1: Button
     private lateinit var button2: Button
     private lateinit var button3: Button
@@ -18,7 +19,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var button6: Button
     private lateinit var button7: Button
     private lateinit var button8: Button
-    private lateinit var button9: Button
+    private lateinit var button9: Button*/
+
+    var oldValue = 0
+    var newValue = 0
+    var currValue = ""
+
+    private lateinit var displayCurr: TextView
 
     private lateinit var buttonPlus: Button
     private lateinit var buttonMinus: Button
@@ -37,7 +44,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button0 = findViewById(R.id.button_0)
+/*        button0 = findViewById(R.id.button_0)
         button1 = findViewById(R.id.button_1)
         button2 = findViewById(R.id.button_2)
         button3 = findViewById(R.id.button_3)
@@ -46,7 +53,9 @@ class MainActivity : AppCompatActivity() {
         button6 = findViewById(R.id.button_6)
         button7 = findViewById(R.id.button_7)
         button8 = findViewById(R.id.button_8)
-        button9 = findViewById(R.id.button_9)
+        button9 = findViewById(R.id.button_9)*/
+
+        displayCurr = findViewById(R.id.textViewDigits)
 
         buttonPlus = findViewById(R.id.button_plus)
         buttonMinus = findViewById(R.id.button_minus)
@@ -54,13 +63,47 @@ class MainActivity : AppCompatActivity() {
         buttonDivide = findViewById(R.id.button_divide)
         buttonModulo = findViewById(R.id.button_percent)
         buttonPlusMinus = findViewById(R.id.button_plusminus)
-
         buttonEqual = findViewById(R.id.button_equal)
         buttonAC = findViewById(R.id.button_AC)
-
         buttonComma = findViewById(R.id.button_comma)
+
 
     }
 
+    fun digitPressed(view: View) {
+        when(view.id)
+        {
+            R.id.button_0 -> numPressed(0)
+            R.id.button_1 -> numPressed(1)
+            R.id.button_2 -> numPressed(2)
+            R.id.button_3 -> numPressed(3)
+            R.id.button_4 -> numPressed(4)
+            R.id.button_5 -> numPressed(5)
+            R.id.button_6 -> numPressed(6)
+            R.id.button_7 -> numPressed(7)
+            R.id.button_8 -> numPressed(8)
+            R.id.button_9 -> numPressed(9)
+        }
+    }
+
+    private fun numPressed(digit: Int){
+        if (displayCurr.text == "H ITS")
+        {
+            currValue = digit.toString()
+            if (digit == 1)
+                displayCurr.text = " $digit"
+            else
+                displayCurr.text = digit.toString()
+        }
+        else if (currValue.length <= 8)
+        {
+            currValue = currValue.plus(digit.toString())
+
+            if (digit == 1)
+                displayCurr.text = displayCurr.text.toString() + " ".plus(digit.toString())
+            else
+                displayCurr.text = displayCurr.text.toString().plus(digit.toString())
+        }
+    }
 
 }
